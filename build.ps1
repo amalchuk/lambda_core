c2faa04683145c89456966acdbdcc7eb829a2a8a
Andrew Malchuk <andrew.malchuk@yandex.ru>
2020-10-07 23:47:06 +0400

    Wed, 07 Oct 2020 19:47:06 +0000

--- build.ps1
+++ build.ps1
@@ -1,9 +1,12 @@
+# Copyright 2020 Andrew Malchuk. All rights reserved.
+# This project is licensed under the terms of the MIT License.
+
 $Global:ProgressPreference = "SilentlyContinue"
 $VirtualEnvironmentPath = Get-Location | Join-Path -ChildPath ".venv"
 $VirtualEnvironmentExists = Test-Path -Path $VirtualEnvironmentPath
 
 if ($VirtualEnvironmentExists -eq $False) {
-  Write-Host "[*] Download the flat assembler 1.73.25 for Windows"
+  Write-Host "Download the flat assembler" -ForegroundColor DarkBlue
 
   $FASMDownloadUrl = "https://flatassembler.net/fasmw17325.zip"
   $FASMDestinationPath = Get-Location | Join-Path -ChildPath "flat_assembler.zip"
@@ -30,18 +33,30 @@ if ($BinaryPathExists -eq $False) {
 
 $SourceFaviconPath = Join-Path -Path $SourceCodePath -ChildPath "favicon" | Join-Path -ChildPath "favicon.asm"
 $BinaryFaviconPath = Join-Path -Path $SourceCodePath -ChildPath "favicon.ico"
-Write-Host "[*] Build $SourceFaviconPath to $BinaryFaviconPath"
-FASM $SourceFaviconPath $BinaryFaviconPath
+Write-Host "Build $SourceFaviconPath to $BinaryFaviconPath" -ForegroundColor DarkBlue
+FASM $SourceFaviconPath $BinaryFaviconPath | Out-Null
+
+if (!$?) {
+  Write-Error -Message "Failed to build the $SourceFaviconPath" -Category ParserError -ErrorAction Stop
+}
 
 $SourceLambdaDLLPath = Join-Path -Path $SourceCodePath -ChildPath "lambda_dll.asm"
 $BinaryLambdaDLLPath = Join-Path -Path $BinaryCodePath -ChildPath "lambda.dll"
-Write-Host "[*] Build $SourceLambdaDLLPath to $BinaryLambdaDLLPath"
-FASM $SourceLambdaDLLPath $BinaryLambdaDLLPath
+Write-Host "Build $SourceLambdaDLLPath to $BinaryLambdaDLLPath" -ForegroundColor DarkBlue
+FASM $SourceLambdaDLLPath $BinaryLambdaDLLPath | Out-Null
+
+if (!$?) {
+  Write-Error -Message "Failed to build the $SourceLambdaDLLPath" -Category ParserError -ErrorAction Stop
+}
 
 $SourceLambdaEXEPath = Join-Path -Path $SourceCodePath -ChildPath "lambda_exe.asm"
 $BinaryLambdaEXEPath = Join-Path -Path $BinaryCodePath -ChildPath "lambda.exe"
-Write-Host "[*] Build $SourceLambdaEXEPath to $BinaryLambdaEXEPath"
-FASM $SourceLambdaEXEPath $BinaryLambdaEXEPath
+Write-Host "Build $SourceLambdaEXEPath to $BinaryLambdaEXEPath" -ForegroundColor DarkBlue
+FASM $SourceLambdaEXEPath $BinaryLambdaEXEPath | Out-Null
 
-Write-Host "[*] Done"
+if (!$?) {
+  Write-Error -Message "Failed to build the $SourceLambdaEXEPath" -Category ParserError -ErrorAction Stop
+}
+
+Write-Host "Done" -ForegroundColor DarkGreen
 $Global:ProgressPreference = "Continue"
